b3196a76e502e3b3a04f1f4de63ea7411de814d3
Miguel de Almeida Correia <miguelalmeidacorreia@gmail.com>
2025-12-06 19:29:35 +0000

    feat: add AuthorRepository

---
+++ src/main/java/org/library/repository/AuthorRepository.java
@@ -0,0 +1,14 @@
+package org.library.repository;
+
+import org.library.entities.Author;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface AuthorRepository extends JpaRepository<Author, Long> {
+
+    Optional<Author> findByName(String name);
+
+}
