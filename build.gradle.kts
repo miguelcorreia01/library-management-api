d7523350adf74ee8ea781052f335e922769f86f5
Miguel de Almeida Correia <miguelalmeidacorreia@gmail.com>
2025-12-06 17:48:13 +0000

    project setup

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,11 @@
 plugins {
     id("java")
+    id("org.springframework.boot") version "3.2.0"
+    id("io.spring.dependency-management") version "1.1.3"
+    id("io.swagger.core.v3.swagger-gradle-plugin") version "2.2.41"
 }
 
-group = "org.example"
+group = "org.library"
 version = "1.0-SNAPSHOT"
 
 repositories {
@@ -12,6 +15,17 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+
+    runtimeOnly("org.postgresql:postgresql")
+
+    implementation("com.auth0:java-jwt:4.4.0")
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
+
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
