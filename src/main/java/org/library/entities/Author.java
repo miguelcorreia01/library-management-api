11c8f4dbec96c1f4ce2cc1a8de988ebb5b76024c
Miguel de Almeida Correia <miguelalmeidacorreia@gmail.com>
2025-12-06 18:32:00 +0000

    feat: add Author entity

---
+++ src/main/java/org/library/entities/Author.java
@@ -0,0 +1,28 @@
+package org.library.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "authors")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String name;
+
+    @OneToMany(mappedBy = "author", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Book> books = new ArrayList<>();
+
+}
