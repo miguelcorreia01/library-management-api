ddc1515f1f3617d8abf241f1d9f44dd9f7c2fd3a
Miguel de Almeida Correia <miguelalmeidacorreia@gmail.com>
2025-12-06 18:32:11 +0000

    feat: add Category entity

---
+++ src/main/java/org/library/entities/Category.java
@@ -0,0 +1,28 @@
+package org.library.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Generated;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "categories")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String name;
+
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Book> books = new ArrayList<>();
+}
