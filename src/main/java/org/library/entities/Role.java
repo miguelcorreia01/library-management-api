1e0e1d0f8c9b8616cf8eb1c86b91114291ddd8cf
Miguel de Almeida Correia <miguelalmeidacorreia@gmail.com>
2025-12-06 18:31:03 +0000

    feat: add User entity with enum-based role

---
+++ src/main/java/org/library/entities/Role.java
@@ -0,0 +1,5 @@
+package org.library.entities;
+
+public enum Role {
+    Admin, Librarian, User
+}
---
+++ src/main/java/org/library/entities/User.java
@@ -0,0 +1,29 @@
+package org.library.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    private String password;
+
+    private boolean isActive;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+}
