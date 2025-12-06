ae3efcc85d69111e311fa7878e0216c527cb4e5d
Miguel de Almeida Correia <miguelalmeidacorreia@gmail.com>
2025-12-06 18:31:38 +0000

    feat: add Book entity

---
+++ src/main/java/org/library/entities/Book.java
@@ -0,0 +1,35 @@
+package org.library.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "books")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private int releaseYear;
+
+    private boolean isBorrowed = false;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "author_id", nullable = false)
+    private Author author;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id", nullable = false)
+    private Category category;
+
+}
