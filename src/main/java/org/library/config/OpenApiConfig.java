c23f762b50593f2718761ec1cfb1716fcee8af63
Miguel de Almeida Correia <miguelalmeidacorreia@gmail.com>
2025-12-10 18:08:41 +0000

    feat(api): add OpenApiConfig for Swagger documentation and security configuration

---
+++ src/main/java/org/library/config/OpenApiConfig.java
@@ -0,0 +1,40 @@
+package org.library.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Library Management API")
+                        .version("1.0")
+                        .description("REST API for Library Management System")
+                        .contact(new Contact()
+                                .name("LibraryManager")
+                                .url("https://github.com/miguelcorreia01/library-management-api"))
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("https://www.apache.org/licenses/LICENSE-2.0.html")))
+                .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
+                .components(new Components()
+                        .addSecuritySchemes("Bearer Authentication", createAPIKeyScheme()));
+    }
+
+    private SecurityScheme createAPIKeyScheme() {
+        return new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .bearerFormat("JWT")
+                .scheme("bearer");
+    }
+}
