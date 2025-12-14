e28da4ee524201318e647d9aa74c7f78c494d77f
Miguel de Almeida Correia <miguelalmeidacorreia@gmail.com>
2025-12-14 21:37:02 +0000

    fix(book): add validation annotations to BookRequest

--- src/main/java/org/library/dto/book/BookRequest.java
+++ src/main/java/org/library/dto/book/BookRequest.java
@@ -1,11 +1,23 @@
 package org.library.dto.book;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class BookRequest {
+    @NotBlank(message = "Book title is required")
     private String title;
-    private long authorId;
-    private long categoryId;
+
+    @NotNull(message = "Author ID is required")
+    private Long authorId;
+    @NotNull(message = "Category ID is required")
+    private Long categoryId;
+
+    @NotNull(message = "Release year is required")
+    @Min(value = 0 , message = "Release year must be a valid year")
+    @Max(value = 2026 , message = "Release year must be a valid year")
     private int releaseYear;
 }
