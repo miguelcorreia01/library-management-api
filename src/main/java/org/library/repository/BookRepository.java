fd9fab4e3fc227db434dcad1a2d89c3a3ea035b1
Miguel de Almeida Correia <miguelalmeidacorreia@gmail.com>
2025-12-14 21:46:39 +0000

    feat(book): add searchBooks method to BookRepository

--- src/main/java/org/library/repository/BookRepository.java
+++ src/main/java/org/library/repository/BookRepository.java
@@ -4,6 +4,8 @@ import org.library.entities.Book;
 import org.library.entities.Category;
 import org.library.entities.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -22,4 +24,14 @@ public interface BookRepository extends JpaRepository<Book, Long> {
     List<Book> findByIsBorrowedTrue();
     List<Book> findByIsBorrowedFalse();
 
+    @Query("SELECT b FROM Book b WHERE " +
+            "(:title IS NULL OR LOWER(b.title) LIKE LOWER(CONCAT('%', :title, '%'))) AND " +
+            "(:authorName IS NULL OR LOWER(b.author.name) LIKE LOWER(CONCAT('%', :authorName, '%'))) AND " +
+            "(:categoryName IS NULL OR LOWER(b.category.name) LIKE LOWER(CONCAT('%', :categoryName, '%'))) AND " +
+            "(:releaseYear IS NULL OR b.releaseYear = :releaseYear)")
+    List<Book> searchBooks(@Param("title") String title,
+                           @Param("authorName") String authorName,
+                           @Param("categoryName") String categoryName,
+                           @Param("releaseYear") Integer releaseYear);
 }
+
