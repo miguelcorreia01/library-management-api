8aab1456cb051aa355bd4d97ef6afb9046b1e135
Miguel de Almeida Correia <miguelalmeidacorreia@gmail.com>
2025-12-14 21:37:30 +0000

    fix(book): add constructors to BookResponse

--- src/main/java/org/library/dto/book/BookResponse.java
+++ src/main/java/org/library/dto/book/BookResponse.java
@@ -1,8 +1,12 @@
 package org.library.dto.book;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class BookResponse {
     private long id;
     private String title;
