362f6c7a77c9d6772360dc8c0428d998282f6124
Miguel de Almeida Correia <miguelalmeidacorreia@gmail.com>
2025-12-14 21:44:45 +0000

    feat(category): add CategoryRequest and CategoryResponse DTOs

---
+++ src/main/java/org/library/dto/category/CategoryResponse.java
@@ -0,0 +1,13 @@
+package org.library.dto.category;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CategoryResponse {
+    private long id;
+    private String name;
+}
